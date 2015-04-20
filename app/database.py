328dcc3ecdd621d2d3e3cf9c0213dac513906d24
alchster <alchster@gmail.com>
2015-04-20 13:49:59 +0300

    roll back to original v0.1

--- app/database.py
+++ app/database.py
@@ -3,12 +3,9 @@ import sqlite3
 import re
 
 re_str = re.compile(r'^[\w\s\(\)-]*$')
-db_query = 'select id, name, ip from bundles where name not like "Auto%%" and name not like "Reserved%%" and name like "%s" %s order by name'
+db_query = 'select name, ip from bundles where name not like "Auto%%" and name not like "Reserved%%" and name like "%s" %s order by name'
 db_path = '/opt/modgud/3.0.0/etc/modgud/configuration.sqlite3'
 db_exclude = 'and name not like "%%%s%%" '
-
-# /opt/modgud/3.0.0/var/log/modgudd.sqlite3
-# select bundle_id from bundle_stats where last_connect > last_disconnect;
 
 def get_list(string=''):
 	if not re_str.match(string) or len(string) > 30:
